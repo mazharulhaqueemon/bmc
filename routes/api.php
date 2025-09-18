aa608cddfa81a6ff2cadafc9e1e54446b0d54540
SabbirAhmad <sabbir.ahmad1@g.bracu.ac.bd>
2025-09-18 20:04:17 +0600

    Chat-phone-request-notification-plan

--- routes/api.php
+++ routes/api.php
@@ -10,8 +10,7 @@ use App\Http\Controllers\API\FirebaseController;
 use App\Http\Controllers\API\ChatController;
 use App\Http\Controllers\API\PlanController;
 use App\Http\Controllers\API\PhoneRequestController;
-use App\Http\Controllers\API\ProfilePictureController;
-use App\Http\Controllers\API\PaymentController;
+
 
 Route::post('signup',[AuthController::class,'signup']);
 Route::post('login',[AuthController::class,'login']);
@@ -30,34 +29,8 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 
-
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/admin/plan', [PlanController::class, 'create']); // create plan
-    Route::get('/admin/plans', [PlanController::class, 'index']);   // list plans
-});
-
-
-
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/phone-requests/send', [PhoneRequestController::class, 'sendRequest']);
     Route::post('/phone-requests/{id}/respond', [PhoneRequestController::class, 'respondRequest']);
     Route::get('/phone-requests', [PhoneRequestController::class, 'listRequests']);
-});
-
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('profile-pictures/upload', [ProfilePictureController::class, 'upload']);
-    Route::get('profile-pictures', [ProfilePictureController::class, 'list']);
-    Route::delete('profile-pictures/{id}', [ProfilePictureController::class, 'delete']);
-    Route::post('profile-pictures/{id}/primary', [ProfilePictureController::class, 'setPrimary']);
-});
-
-
-Route::middleware('auth:sanctum')->group(function() {
-    // User submits payment request
-    Route::post('/payment/submit', [PaymentController::class, 'submit']);
-    
-    // User lists their own payments
-    Route::get('/payment/user', [PaymentController::class, 'userPayments']);
 });
